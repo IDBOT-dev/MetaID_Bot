e41cbd26b4705294276aa34655147c82dca6e00c
riverrun46 <river_run@126.com>
2024-10-11 18:06:54 +0800

    push

--- src/ticket/ticket.actions.ts
+++ src/ticket/ticket.actions.ts
@@ -1,9 +1,10 @@
 import { Command, Ctx, Sender, Update } from 'nestjs-telegraf'
 import { FullContext } from '../interfaces/context.interface'
 import { buyTicket, sellTicket } from 'src/lib/ticket'
-import { getAddress, getPublicKey, getSigner } from 'src/lib/metaid'
+import { getAddress, getCred, getPublicKey, getSigner } from 'src/lib/metaid'
 import { UserService } from 'src/user.service'
 import { messages } from 'src/lib/messages'
+import { ticketMessage } from 'src/lib/util'
 
 @Update()
 export class TicketActions {
@@ -20,11 +21,18 @@ export class TicketActions {
       return
     }
 
+    const [tick, priceInBtc] = ctx.payload.split(' ')
+    if (!tick || !priceInBtc) {
+      await ctx.reply('Invalid')
+      return
+    }
+
     const address = getAddress(user.mneid, user.path)
     const publicKey = getPublicKey(user.mneid, user.path)
     const signer = getSigner(user.mneid, user.path)
+    const cred = await getCred(user.mneid, user.path, ticketMessage)
 
-    const res = await buyTicket(ctx.payload, address, publicKey, signer)
+    const res = await buyTicket(ctx.payload, address, publicKey, signer, cred)
 
     await ctx.reply(res)
   }
@@ -40,11 +48,18 @@ export class TicketActions {
       return
     }
 
+    const [tick, priceInBtc] = ctx.payload.split(' ')
+    if (!tick || !priceInBtc) {
+      await ctx.reply('Invalid')
+      return
+    }
+
     const address = getAddress(user.mneid, user.path)
     const publicKey = getPublicKey(user.mneid, user.path)
     const signer = getSigner(user.mneid, user.path)
+    const cred = await getCred(user.mneid, user.path, ticketMessage)
 
-    const res = await sellTicket(ctx.payload, address, publicKey, signer)
+    const res = await sellTicket(ctx.payload, address, publicKey, signer, cred)
 
     await ctx.reply(res)
   }
